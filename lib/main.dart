634c5931a8177d6dac71964c73bb924899fac198
Guiffou-Joël <guiffoujoel@gmail.com>
2021-04-01 12:51:17 +0100

    Ball widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Pong Game",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
